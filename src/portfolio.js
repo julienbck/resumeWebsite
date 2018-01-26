049080927b60362327f8aa70cd6deca251cce9ae
Julien BOCK <julien.bock@decathlon.com>
2018-01-26 12:55:31 +0100

    update

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,33 +1,66 @@
 import React, { Component } from 'react';
 import './App.css';
+import Lightbox from 'react-image-lightbox';
 
-const modal = document.getElementById('myModal');
-const img = document.getElementById('myImg');
-const modalImg = document.getElementById("img01");
-const captionText = document.getElementById("caption");
-const span = document.getElementsByClassName("closeImg")[0];
+
+const images = [
+  'https://wallpaperstudio10.com/static/wpdb/wallpapers/1920x1080/172445.jpg',
+  'https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-240723.jpg4',
+  'BackgroundResume.png'
+];
 
 class Portfolio extends Component {
 
-onClickImg = () => {
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-}
-
-// Get the <span> element that closes the modal
-// When the user clicks on <span> (x), close the modal
-onClickSpan = () => {
-  modal.style.display = "none";
-}
+  constructor(props) {
+    super(props);
+    this.state = {
+      photoIndex: 0,
+      isOpen: false,
+    };
+  }
+  onClickOnImage = () => {
+    this.setState({
+      isOpen: true,
+    });
+  }
+  onClickOnImage2 = () => {
+    this.setState({
+      isOpen: true,
+      photoIndex: 1,
+    })
+  }
+  onClickOnImage3 = () => {
+    this.setState({
+      isOpen: true,
+      photoIndex: 2,
+    })
+  }
   render(){
+    const { photoIndex, isOpen } = this.state;
     return(
       <section className= {this.props.portfolioVisuel}>
       <a onClick={this.props.backToHome} className="close"></a>
       <div className="ContentPartLeft">
+      <div>
+        {isOpen && (
+          <Lightbox
+            mainSrc={images[photoIndex]}
+            nextSrc={images[(photoIndex + 1)]}
+            prevSrc={images[(photoIndex- 1)]}
+            onCloseRequest={() => this.setState({ isOpen: false })}
+          />
+        )}
+      </div>
         <div className="GridPortfolioContent">
           <div className="RowPortfolio">
 
               <img src='https://wallpaperstudio10.com/static/wpdb/wallpapers/1920x1080/172445.jpg'></img>
+              <img onClick={this.onClickOnImage} src='https://wallpaperstudio10.com/static/wpdb/wallpapers/1920x1080/172445.jpg'></img>
+              <img onClick={this.onClickOnImage2} src='https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-240723.jpg'></img>
+          </div>
+          <div className="RowPortfolio">
+
+              <img onClick={this.onClickOnImage3} src='./BackgroundContent.png'></img>
               <img src='https://wallpaperstudio10.com/static/wpdb/wallpapers/1920x1080/172445.jpg'></img>
               <img src='https://wallpaperstudio10.com/static/wpdb/wallpapers/1920x1080/172445.jpg'></img>
           </div>
@@ -39,19 +72,9 @@ onClickSpan = () => {
           </div>
           <div className="RowPortfolio">
 
-              <img src='https://wallpaperstudio10.com/static/wpdb/wallpapers/1920x1080/172445.jpg'></img>
-              <img src='https://wallpaperstudio10.com/static/wpdb/wallpapers/1920x1080/172445.jpg'></img>
-              <img src='https://wallpaperstudio10.com/static/wpdb/wallpapers/1920x1080/172445.jpg'></img>
+
           </div>
-          <div className="RowPortfolio">
-            <img className="myImg" src="https://wallpaperstudio10.com/static/wpdb/wallpapers/1920x1080/172445.jpg" alt="Trolltunga, Norway" width="20%" height="20%" onClick={this.onClickImg}></img>
-            <div className="myModal" className="modal">
-              <span className="closeImg" onClick={this.onClickSpan}>&times;</span>
-              <img className="modal-content" className="img01"></img>
-              <div className="caption"></div>
-            </div>
-          </div>
-          </div>
+        </div>
       </div>
 
       <div className="ContentPartRight">
